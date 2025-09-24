40b028bca73a71375b3c3a1302842c4c38c7e3d1
Lucas Emanuel <lucasesn23@gmail.com>
2025-09-24 04:15:22 -0300

    Feat/visual improvment (#13)
    * feat: improving ux
    
    * ajustando comportamento ao finalizar pilha
    
    * feature: better ux
    
    * Update NewsCard.vue
    
    ---------
    
    Co-authored-by: Lucas Emanuel <lucaesn23@gmail.com>

--- app/components/feed/NewsCard.vue
+++ app/components/feed/NewsCard.vue
@@ -1,14 +1,14 @@
 <template>
   <li :class="'p-6 rounded-3xl bg-yellowSoft dark:bg-secondary-light border-2 border-yellow-600 dark:border-secondary-dark mb-0 h-[95%] w-[95%] relative'">
-    <span class="text-sm font-semibold mb-3 px-2 py-1 bg-slate-600 text-white rounded-full inline-block mr-2">
-      {{ state.currentCategory }}
-    </span>
-    <span class="text-sm font-semibold mb-3 px-2 py-1 bg-black dark:bg-primary-light text-white rounded-full inline-block">
+    <div class="px-2 py-0 text-white text-lg font-bold rounded-full inline-block mr-2" :style="{ backgroundColor: state.currentCategory.color }">
+      <Icon :name="state.currentCategory.icon" class="-mb-1"/>
+    </div>
+    <span class="text-sm font-semibold mb-3 px-2 py-1 bg-secondary dark:bg-primary-light text-white rounded-full inline-block">
       {{ formatedDate(pubDate) }}
     </span>
     <div class="rounded-md py-3 flex justify-between mb-3">
       <div class="flex gap-4">
-        <div class="inline-block h-8 w-8 rounded-full ring-4 ring-white">
+        <div class="inline-flex h-8 w-8 rounded-full ring-4 ring-white">
           <img v-if="!source.image?.url" src="../../assets/images/flower-icon-2048x2048-gydxzqnj.png" alt="Fonte padrão">
           <img v-else :src="source.image?.url" :alt="`Logo de ${source?.title ?? 'fonte'}`" class="min-w-full">
         </div>
@@ -22,7 +22,7 @@
         </div>
       </div>
       <a :href="link" target="_blank"
-        class="rounded-full flex font-bold text-white bg-slate-900 dark:bg-primary-light border-2 border-slate-900 dark:border-primary-light px-3 py-1 hover:bg-slate-600 hover:text-slate-300">
+        class="rounded-full flex font-bold text-white bg-secondary dark:bg-primary-light border-2 border-slate-900 dark:border-primary-light px-3 py-1 hover:bg-slate-600 hover:text-slate-300">
         Ler →
       </a>
     </div>
